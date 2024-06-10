aa484b04dadbde76d883f3ae73e8ba39a06eeadc
Dinuja Pinto <dpinto@caresystemsinc.com>
2024-06-10 23:40:24 +0530

    second commit

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../type';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private htttpClient: HttpClient
+  ) { }
+
+  get<T>(url: string, options:Options):Observable<T>{
+    return this.htttpClient.get<T>(url, options) as Observable<T>;
+  }
 }
