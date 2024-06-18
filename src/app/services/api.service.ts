83503b3d57f1a8ca9b8ac5746346aa8f1f9a3337
Dinuja Pinto <sri.prabasha@gmail.com>
2024-06-18 22:59:04 +0530

    cud implement

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -15,4 +15,16 @@ export class ApiService {
   get<T>(url: string, options:Options):Observable<T>{
     return this.htttpClient.get<T>(url, options) as Observable<T>;
   }
+
+  post<T>(url: string, body: any, options:Options):Observable<T>{
+    return this.htttpClient.post<T>(url, body, options) as Observable<T>;
+  }
+
+  put<T>(url: string, body: any, options:Options):Observable<T>{
+    return this.htttpClient.put<T>(url, body, options) as Observable<T>;
+  }
+
+  delete<T>(url: string, options:Options):Observable<T>{
+    return this.htttpClient.delete<T>(url, options) as Observable<T>;
+  }
 }
