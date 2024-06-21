510a1047dfc8ec3f2cb663844a10776aaa289648
Dinuja Pinto <sri.prabasha@gmail.com>
2024-06-21 15:18:08 +0530

    edit-popup v1

--- src/app/components/edit-popup/edit-popup.component.ts
+++ src/app/components/edit-popup/edit-popup.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../../type';
 
 @Component({
   selector: 'app-edit-popup',
   standalone: true,
-  imports: [],
+  imports: [DialogModule, CommonModule],
   templateUrl: './edit-popup.component.html',
   styleUrl: './edit-popup.component.css'
 })
 export class EditPopupComponent {
+  @Input() display: boolean = false;
+  @Output() confirm = new EventEmitter<Product>();
 
+  @Input() product: Product = {
+    name: '',
+    image:'',
+    price:'',
+    rating:0
+  };
+
+  onConfirm(){
+    this.confirm.emit(this.product);
+  }
+
+  onCancel(){
+    this.display = false;
+  }
 }
