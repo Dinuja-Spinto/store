69d29f7d35c29f5cef8e10815e41ec6d385ef68e
Dinuja Pinto <sri.prabasha@gmail.com>
2024-06-22 22:04:52 +0530

    confirm box

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,14 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { Product } from '../../../type';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmationService } from 'primeng/api';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [RatingModule, FormsModule, ButtonModule],
+  imports: [RatingModule, FormsModule, ButtonModule, ConfirmPopupModule],
   providers:[ConfirmationService],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
@@ -16,12 +17,15 @@ import { ConfirmationService } from 'primeng/api';
 export class ProductComponent {
   constructor(private confirmationService: ConfirmationService) {}
 
+  @ViewChild('deleteButton') deleteButton: any;
+
   @Input() product!: Product;
   @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
   @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
 
   confirmDelete(){
     this.confirmationService.confirm({
+      target: this.deleteButton.nativeElement,
       message: 'Are you sure that you want to delete this product?',
       accept:() =>{
         this.deleteProduct();
